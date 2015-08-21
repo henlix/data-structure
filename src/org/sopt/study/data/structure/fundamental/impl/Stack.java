efdd000218c6e3c480b4bfa1356774fc598d8548
henlix <niah_raszagal@live.com>
2015-08-21 21:18:04 +0900

    Circular Queue 추가되었습니다.

--- src/org/sopt/study/data/structure/fundamental/impl/Stack.java
+++ src/org/sopt/study/data/structure/fundamental/impl/Stack.java
@@ -10,7 +10,9 @@ public class Stack<T> extends Collection<T> {
     public void push(Node<T> node) {
 
         list.add(node);
-        size++;
+
+        // TODO : 코드 변경 필요.
+        size += 1;
     }
 
     public Node<T> pop() {
